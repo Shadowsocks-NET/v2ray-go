22d63870f025ae710afa415efbf759ca14b0031f
秋のかえで <autmaple@protonmail.com>
2021-09-24 13:04:31 +0800

    🔧 Fix Trojan fallback cannot get TLS ALPN

--- proxy/trojan/server.go
+++ proxy/trojan/server.go
@@ -5,7 +5,6 @@ package trojan
 
 import (
 	"context"
-	"crypto/tls"
 	"io"
 	"strconv"
 	"time"
@@ -25,6 +24,7 @@ import (
 	"github.com/Shadowsocks-NET/v2ray-go/v4/features/policy"
 	"github.com/Shadowsocks-NET/v2ray-go/v4/features/routing"
 	"github.com/Shadowsocks-NET/v2ray-go/v4/transport/internet"
+	"github.com/Shadowsocks-NET/v2ray-go/v4/transport/internet/tls"
 	"github.com/Shadowsocks-NET/v2ray-go/v4/transport/internet/udp"
 )
 
